df9ccee4d277de554d5e28720c10ab806b963065
Patricia Camiansky <patricia.camiansky@paradoxinteractive.com>
2024-05-05 22:49:33 +0200

    Change update_package_price to: - set the year to current year if not provided; - check if there is a previous price history for the package, amount and municipality combination and, if not, create it - save the new price to history

--- app/lib/update_package_price.rb
+++ app/lib/update_package_price.rb
@@ -3,11 +3,20 @@
 class UpdatePackagePrice
   def self.call(package, new_price_cents, municipality, **options)
     Package.transaction do
-      # Add a pricing history record
-      Price.create!(package: package, amount_cents: package.amount_cents, municipality: municipality)
-
-      # Update the current price
+      year = options[:year].nil? ? Date.today.year : options[:year]
+      # Add a pricing history record for previous price, if package has a price and no history exists for that package,
+      # amount and municipality combination
+      if package.amount_cents > 0 and Price.where(
+          package: package,
+          amount_cents: package.amount_cents,
+          municipality: municipality,
+        ).empty?
+        Price.create!(package: package, amount_cents: package.amount_cents, municipality: municipality, year: year)
+      end
+      # Update the current package price to match the new price
       package.update!(amount_cents: new_price_cents)
+      # Save the new price to history
+      Price.create!(package: package, amount_cents: new_price_cents, municipality: municipality, year: year)
     end
   end
 end
